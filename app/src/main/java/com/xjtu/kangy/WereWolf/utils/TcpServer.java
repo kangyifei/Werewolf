4de8126d3807357a0795f8e3481c096c442c9821
kangyifei <530498334@qq.com>
2017-09-05 14:10:14 +0800

    fix traivs config

--- app/src/main/java/com/xjtu/kangy/WereWolf/utils/TcpServer.java
+++ app/src/main/java/com/xjtu/kangy/WereWolf/utils/TcpServer.java
@@ -1,8 +1,25 @@
 package com.xjtu.kangy.WereWolf.utils;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by kangy on 2017/9/4.
  */
 
 public class TcpServer {
+    ServerSocket mServerSocket;
+
+    public TcpServer(int port) throws IOException {
+        this.mServerSocket = new ServerSocket(port);
+    }
+
+    void connect() {
+        try {
+            Socket socket = mServerSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
